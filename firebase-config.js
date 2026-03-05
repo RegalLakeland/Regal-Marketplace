2d080a51728aadadb59112513f928d7290755221
Regal Lakeland <mhallhonda@gmail.com>
2026-03-05 09:33:01 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyB6IAiH6zILQKuJRuXc55Q4hEX8q6F2kxE",
+  authDomain: "regal-lakeland-marketplace.firebaseapp.com",
+  projectId: "regal-lakeland-marketplace",
+  storageBucket: "regal-lakeland-marketplace.firebasestorage.app",
+  messagingSenderId: "1014346693296",
+  appId: "1:1014346693296:web:fc76118d1a8db347945975"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
