cd3700753a7e6bea141ece208e3601e227a91685
Regal Lakeland <mhallhonda@gmail.com>
2026-03-05 17:17:36 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -19,10 +19,11 @@ window.ADMIN_EMAILS = [
 
 // Firebase Web App config (REQUIRED)
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_PROJECT_ID_HERE",
-  storageBucket: "PASTE_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_SENDER_ID_HERE",
-  appId: "PASTE_APP_ID_HERE",
+  apiKey: "AIzaSyB6IAiH6zILQKuJRuXc55Q4hEX8q6F2kxE",
+  authDomain: "regal-lakeland-marketplace.firebaseapp.com",
+  projectId: "regal-lakeland-marketplace",
+  storageBucket: "regal-lakeland-marketplace.firebasestorage.app",
+  messagingSenderId: "1014346693296",
+  appId: "1:1014346693296:web:fc76118d1a8db347945975"
 };
+
