ae4904cf90fe9d15cb8557e1714a075e23667588
Regal Lakeland <mhallhonda@gmail.com>
2026-03-05 22:53:37 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,20 @@
-// Firebase Config
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyB6IAiH6zILQKuJRuXc55Q4hEX8q6F2kxE",
   authDomain: "regal-lakeland-marketplace.firebaseapp.com",
   projectId: "regal-lakeland-marketplace",
-
-  // FIXED STORAGE BUCKET
   storageBucket: "regal-lakeland-marketplace.appspot.com",
-
   messagingSenderId: "1014346693296",
   appId: "1:1014346693296:web:fc76118d1a8db347945975"
 };
 
-// Allowed admin emails
-export const ADMIN_EMAILS = new Set([
+const ADMIN_EMAILS = [
   "michael.h@regallakeland.com",
   "janni.r@regallakeland.com",
   "chrissy.h@regallakeland.com",
   "amy.m@regallakeland.com"
-]);
+];
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
