ed22c107c88ee4dd8e9b640558f7d40af25e34b4
Regal Lakeland <mhallhonda@gmail.com>
2026-03-06 22:11:51 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,49 +1,16 @@
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, sendPasswordResetEmail, updateProfile } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
-import { getFirestore, collection, addDoc, getDocs, doc, getDoc, updateDoc, deleteDoc, query, where, orderBy, Timestamp, setDoc } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
-import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB6IAiH6zILQKuJRuXc55Q4hEX8q6F2kxE",
-  authDomain: "regal-lakeland-marketplace.firebaseapp.com",
-  projectId: "regal-lakeland-marketplace",
-  storageBucket: "regal-lakeland-marketplace.firebasestorage.app",
-  messagingSenderId: "1014346693296",
-  appId: "1:1014346693296:web:fc76118d1a8db347945975"
+export const firebaseConfig = {
+apiKey: "AIzaSyB6IAiH6zILQKuJRuXc55Q4hEX8q6F2kxE",
+authDomain: "regal-lakeland-marketplace.firebaseapp.com",
+projectId: "regal-lakeland-marketplace",
+storageBucket: "regal-lakeland-marketplace.firebasestorage.app",
+messagingSenderId: "1014346693296",
+appId: "1:1014346693296:web:fc76118d1a8db347945975"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export {
-    app,
-    auth,
-    db,
-    storage,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged,
-    sendPasswordResetEmail,
-    updateProfile,
-    collection,
-    addDoc,
-    getDocs,
-    doc,
-    getDoc,
-    updateDoc,
-    deleteDoc,
-    query,
-    where,
-    orderBy,
-    Timestamp,
-    ref,
-    uploadBytes,
-    getDownloadURL,
-    deleteObject,
-    setDoc
-};
+export const ADMIN_EMAILS = [
+"michael.h@regallakeland.com",
+"janni.r@regallakeland.com",
+"chrissy.h@regallakeland.com",
+"amy.m@regallakeland.com"
+];
